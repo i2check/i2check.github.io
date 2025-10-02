8f69c53b37691fbf8fe9efb144ff78b752f54855
i2check <astrajingga044@gmail.com>
2025-10-02 11:22:37 +0700

    Update package tambah next build

--- zyanmalik/next.config.js
+++ zyanmalik/next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/portfolio',
-  assetPrefix: '/portfolio/'
+  basePath: '/zyanmalik',
+  assetPrefix: '/zyanmalik/'
 }
 
 module.exports = nextConfig
